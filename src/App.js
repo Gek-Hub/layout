f4944e5f9459dbbbebe75050bdb9993ddaf8ee6c
Gek <gek33cr@gmail.com>
2022-07-25 14:43:42 +0300

    create heager

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='App-header'>
+        <div className='panel-top'>
+          <div>
+            <img src='./image/icon/logo.png' alt='logo'/>
+          </div>
+          <div>
+            <div>
+              <img src='./image/icon/store.png' alt='store'/>
+              Колоды
+            </div>
+            <div>
+              <img src='./image/icon/favorites.png' alt='favorites'/>
+              Рейтинг
+            </div>
+
+          </div>
+          <div>
+            <select>
+              <option>ru</option>
+              <option>en</option>
+            </select>
+          </div>
+          <div>
+            <select>
+              <option>Pal Palich</option>
+            </select>
+          </div>
+        </div>
+        
+        <div className="wellcome">
+          <div>
+            <p>Welcome to the service for memorizing words in foreign languages using flashcards</p>
+          </div>
+          <div>
+            <img src="image/china.png" alt="translate-icon" />
+          </div>
+          <div>
+            <button className="btn-getStart" >Get started</button>
+          </div>
+        </div>
+
+        <div className="App-image">
+          <img src="./image/woman.svg" alt='woman'/>
+        </div>
+
+      </div>
     </div>
   );
 }

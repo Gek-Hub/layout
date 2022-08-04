bb46b44ec43ea1e36dc01701a9990916679e2095
Gek <gek33cr@gmail.com>
2022-08-04 15:56:01 +0300

    upgrade my skills

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import './styles/styles.css';
 
 function App() {
+
   return (
     <div className="App">
-      <div className="App-bg"/>
-      <div className="App-image"/>
+      <div className="App-bg" />
+      <div className="App-image" />
       <div className='header'>
         <header>
           <div className='panel-top'>
-            <div className='logo'>
+            <div className='link-container'>
+              <div className='logo'>
+              </div>
+              <div className='deck-rating'>
+                <div className='deck-icon' />
+                Колоды
+                <div className='rating-icon' />
+                Рейтинг
+              </div>
             </div>
-            <div className='deck-rating'> 
-            <div className='deck-icon'/>
-              Колоды 
-            <div className='rating-icon'/>
-             Рейтинг
-            </div>
-             
-            
-            <div className='panel-top'> 
-              <select className='select-languages'>
+
+
+            <div className='select-container'>
+              <select className='select-container select-languages'>
                 <option>ru</option>
                 <option>en</option>
               </select>
-            </div>
-            <div className='panel-top'>
-              <select className="select-accounts">
+
+              <select className="select-container select-accounts">
                 <option>Pal Palich</option>
               </select>
             </div>
@@ -35,13 +37,13 @@ function App() {
           <div>
             <p>Welcome to the service for memorizing words in foreign languages using flashcards</p>
           </div>
-          <div className='translate-icon'/>
+          <div className='translate-icon' />
           <div>
             <button className="btn-getStart" >Get started</button>
           </div>
         </div>
       </div>
-      
+
     </div>
   );
 }
